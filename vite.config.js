fcba232fa11ba759c5e8437ab6d97546f4229107
kondrse <kondor85@mail.ru>
2026-01-14 15:33:48 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { federation } from '@module-federation/vite';
+import federation from '@originjs/vite-plugin-federation';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
