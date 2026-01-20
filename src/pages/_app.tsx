8cd3974fcb7a7e7eec0735ef7138d2612872ad3c
kondrse <kondor85@mail.ru>
2026-01-21 02:22:37 +0300

    change text

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,8 +22,8 @@ export default function HomePage() {
   return (
     <div className="">
       <div className="text-center p-5">
-        <h1 className="text-2xl text-pink-700">Welcome to the Edit Route Image Microfrontend</h1>
-        <p>This is the home page of the shop app, running on its own.</p>
+        <h1 className="text-2xl text-pink-700">Welcome to the IClimbedCard</h1>
+        <p>This is the home page of the microfrontend app, running on its own.</p>
         <h2 className="text-xl text-pink-700">DEMO:</h2>
       </div>
       <EditImage
